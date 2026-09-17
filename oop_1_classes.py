5a61286463fc9fe5469a0658740daa6c7faf0d2f
chaitanya-link <chaitu11106@gmail.com>
2026-09-17 20:03:04 +0530

    classes full

--- oop_1_classes.py
+++ oop_1_classes.py
@@ -3,6 +3,9 @@ class Agent:
         self.name = name      # instance attribute
         self.role = role      # instance attribute
 
+    def change_role(self, new_role):
+        self.role = new_role   # now properly indented inside the method
+
     def introduce(self):      # instance method
         print(f"Hi, I'm {self.name}, and I work as a {self.role}.")
 
@@ -10,7 +13,12 @@ class Agent:
 # Creating objects (instances) from the Agent class
 agent1 = Agent("Researcher-Bot", "web researcher")
 agent2 = Agent("Coder-Bot", "code generator")
-
+agent3 = Agent("Farmer", "Code farmer")
 
 agent1.introduce()
 agent2.introduce()
+agent3.introduce()
+
+# testing change_role
+agent3.change_role("Senior Code Farmer")
+agent3.introduce()
